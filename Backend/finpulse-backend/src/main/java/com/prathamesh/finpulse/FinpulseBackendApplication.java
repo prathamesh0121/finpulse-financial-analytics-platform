75f0e891de2a583409f77c44e0d052b2e7502129
Prathamesh Tambekar <prathameshtambekar9425@gmail.com>
2026-08-05 20:10:12 +0530

    Added Spring Boot backend project

---
+++ Backend/finpulse-backend/src/main/java/com/prathamesh/finpulse/FinpulseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.prathamesh.finpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinpulseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinpulseBackendApplication.class, args);
+	}
+
+}
